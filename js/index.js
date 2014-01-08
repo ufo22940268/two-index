60d1325d2e63533559e926e81a16bab041b6b5ec
ufo22940268 <ufo22940268@gmail.com>
2014-01-08 14:34:16 +0800

    up

--- js/index.js
+++ js/index.js
@@ -11,5 +11,12 @@ $(function() {
             $(e).removeClass("active");
         });
     });
+
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
 });
 
